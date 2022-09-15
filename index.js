1ac054babcb865bb3b9d0238a4531650aa89bfa8
James Medd <hello@jamesmedd.co.uk>
2022-09-15 10:50:19 +0100

    Add some games to the list and some of that custom config goodness

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const attractLengths = require('./attractLengths.json')
 const locateRoms = require('./locateRoms.js')
 const selectCore = require('./selectCore.js')
+const fs = require('fs')
 const os = require('os')
 const { spawn } = require('child_process')
 
@@ -9,16 +11,21 @@ const corePath = `${homePath}/Library/Application Support/RetroArch/cores/`
 
 const args = [...process.argv.slice(2)]
 
-const romsPath = args[0]
+const romsPath = args[0] + (args[0].slice(-1) !== '/' ? '/' : '')
+fs.writeFileSync(`${romsPath}/customConfig.cfg`, `video_font_enable = "false"\npause_nonactive = "false"\nsavestate_auto_load = "true"\nsavestate_directory = "${romsPath}"`)
 
 const roms = locateRoms(romsPath)
 
 function randomGame () {
-  const selectedRom = parseInt(Math.random() * roms.length)
-  const romExtension = roms[selectedRom].match(/\.\S+/)
+  const selectedRom = roms[parseInt(Math.random() * roms.length)]
+  const playLength = attractLengths[selectedRom]
+  const romExtension = selectedRom.match(/\.\S+/)
   const useCore = selectCore(romExtension[0])
-  const emulation = spawn(retroarchPath, ['--fullscreen', '-L', `${corePath}${useCore.core}`, `${romsPath}/${roms[selectedRom]}`])
-  setTimeout(loadAnother, 10000, emulation)
+  const emulation = spawn(retroarchPath, [`--appendconfig="${romsPath}/customConfig.cfg"`, '-L', `"${corePath}${useCore.core}"`, `"${romsPath}${selectedRom}"`], {
+    stdio: 'inherit',
+    shell: true
+  })
+  setTimeout(loadAnother, playLength, emulation)
 }
 
 function loadAnother (process) {
