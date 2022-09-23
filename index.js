3c442da19b024e578bc68384a7ef64ccc2e3e1de
James Medd <hello@jamesmedd.co.uk>
2022-09-23 15:51:48 +0100

    Start work on Pi compatibility

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const UniqueRandom = require('./UniqueRandom')
 const attractLengths = require('./attractLengths.json')
 const locateRoms = require('./locateRoms.js')
 const selectCore = require('./selectCore.js')
+const os = require('os')
 const fs = require('fs')
 const { spawn } = require('child_process')
 const kill = require('tree-kill')
 
+require('dotenv').config()
 const retroarchPath = process.env.RETROARCH_PATH
+const homePath = os.homedir()
+
+const corePath = `${homePath}/.config/retroarch/cores`
 
 const args = [...process.argv.slice(2)]
 
@@ -15,16 +20,16 @@ fs.writeFileSync(`${romsPath}/customConfig.cfg`, `video_font_enable = "false"\np
 
 const roms = locateRoms(romsPath)
 
-console.debug(roms)
-
 const random = new UniqueRandom(roms.length)
+
+console.log(homePath)
 
 function randomGame () {
   const selectedRom = roms[random.new]
   const playLength = attractLengths[selectedRom]
   const romExtension = selectedRom.match(/\.\S+/)
   const useCore = selectCore(romExtension[0])
-  const emulation = spawn(retroarchPath, ['-f', `--appendconfig="${romsPath}/customConfig.cfg"`, '-L', `"${useCore.core}"`, `"${romsPath}${selectedRom}"`], {
+  const emulation = spawn(retroarchPath, ['-f', `--appendconfig="${romsPath}/customConfig.cfg"`, '-L', `"${corePath}/${useCore.core}.so"`, `"${romsPath}${selectedRom}"`], {
     stdio: 'inherit',
     shell: true
   })
