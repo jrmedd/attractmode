993f99c9291b24276ad830715a4d0a60a2901cf2
jrmedd <hello@jamesmedd.co.uk>
2022-09-30 12:59:38 +0100

    Basic Pi compatibility and better randomness

--- UniqueRandom.js
+++ UniqueRandom.js
@@ -1,15 +1,18 @@
 class UniqueRandom {
-  constructor (max) {
+  constructor (max, bufferSize) {
     this.max = max
-    this.last = 0
+    this.buffer = new Array(bufferSize).fill(-1)
   }
 
   get new () {
     let newNumber
     do {
       newNumber = parseInt(Math.random() * this.max)
-    } while (newNumber === this.last)
+    } while (this.buffer.includes(newNumber))
     this.last = newNumber
+    this.buffer.map((value, index) => {
+      index < (this.buffer.length - 1)  ? this.buffer[index] = this.buffer[index + 1] : this.buffer[index] = newNumber
+    })
     return newNumber
   }
 }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const locateRoms = require('./locateRoms.js')
 const selectCore = require('./selectCore.js')
 const os = require('os')
 const fs = require('fs')
-const { spawn } = require('child_process')
+const { spawn, execSync } = require('child_process')
 const kill = require('tree-kill')
 
 require('dotenv').config()
@@ -20,25 +20,26 @@ fs.writeFileSync(`${romsPath}/customConfig.cfg`, `video_font_enable = "false"\np
 
 const roms = locateRoms(romsPath)
 
-const random = new UniqueRandom(roms.length)
-
-console.log(homePath)
+const random = new UniqueRandom(roms.length, parseInt(roms.length/2))
 
 function randomGame () {
   const selectedRom = roms[random.new]
-  const playLength = attractLengths[selectedRom]
+  const playLength = attractLengths[selectedRom] ?? 30000
   const romExtension = selectedRom.match(/\.\S+/)
   const useCore = selectCore(romExtension[0])
   const emulation = spawn(retroarchPath, ['-f', `--appendconfig="${romsPath}/customConfig.cfg"`, '-L', `"${corePath}/${useCore.core}.so"`, `"${romsPath}${selectedRom}"`], {
     stdio: 'inherit',
     shell: true
   })
+  const focus = spawn('wmctrl', [' -a',' RetroArch'],{shell:true})
   setTimeout(loadAnother, playLength, emulation.pid)
 }
 
 function loadAnother (previousPid) {
   randomGame()
+  const focus = spawn('wmctrl', [' -a',' RetroArch'],{shell:true})
   setTimeout(() => kill(previousPid), 250)
 }
 
 randomGame()
+
